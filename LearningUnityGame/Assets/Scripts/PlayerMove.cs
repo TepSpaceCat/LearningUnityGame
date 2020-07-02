94673f8e1c0b66fe2b29ea5aa2182184da1e748a
TehSpaceCat <jhf8fjb@virginia.edu>
2020-07-02 16:57:56 -0400

    brackey's videos 1-5

---
+++ LearningUnityGame/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+
+    public Rigidbody rb;
+    public float ForwardForce = 2000;
+    public float SidewaysForce = 500;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.AddForce(0, 0, ForwardForce * Time.deltaTime);
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(SidewaysForce * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-SidewaysForce * Time.deltaTime, 0, 0);
+        }
+    }
+}
